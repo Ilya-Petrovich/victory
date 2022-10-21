a70fae09515c74f0b82681899e14e13506ab0b72
Ilya Alekseev <ipteacher@mail.ru>
2022-10-21 08:50:47 +0300

    Created index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Регистрация</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+</head>
+<body>
+	<main>
+	    <form action="registration_script.php" method="post">
+	        <h1>Регистрация команды</h1>
+	        <div>
+	            <label for="username">Название команды:</label>
+	            <input type="text" name="username" id="username">
+	        </div>
+	        <button type="submit" name="submit">Зарегистрировать</button>
+	    </form>
+	</main>
+</body>
+</html>
