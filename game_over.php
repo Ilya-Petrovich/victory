d73490b3af02986128d784f878fa647e13baf6b7
Ilya Alekseev <ipteacher@mail.ru>
2022-10-25 20:29:31 +0300

    added game_over.php

--- game_over.php
+++ game_over.php
@@ -7,18 +7,20 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@200&family=Inter&family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="levels/style.css">
     <title>Конец игры</title>
 </head>
 <body>
-    <header class="head__hard">
+    <header class="head__hard" style="width:100%; height:100vh">
         <div class="container">
             <div class="navigation">
                 <div class="nav__left">
-                    <a href="" class="home">На главную</a>
-                    <a href="">Мой прогресс</a>
+                    <!-- <a href=".." class="home">На главную</a> -->
+                    <a href="personal_page.php">Мой прогресс</a>
                 </div>
-                <a href="" class="exit">Выйти</a>
+				<form action="index.php" method="post">
+					<button type="submit" name="submit_exit">Выйти</button>
+				</form>
             </div>
             <div class="end__title">
                 GAME OVER
@@ -29,9 +31,9 @@
             <div class="end__img">
                 <img src="/images/sad_cosmo 1.png" alt="">
             </div>
-            <div class="btn__restart">
+            <!-- <div class="btn__restart">
                 <a href="">Попровать снова</a>
-            </div>
+            </div> -->
         </div>
     </header>
 </body>
