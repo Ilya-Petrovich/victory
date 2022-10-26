b263320e98f1340e6f915d36d31bb66ac692e33b
Dasha Boxerka <ideliya3004@gmail.com>
2022-10-26 15:30:25 +0300

    Changed registration_script.php

--- registration_script.php
+++ registration_script.php
@@ -23,10 +23,11 @@
 			}
     		// echo "Вы успешно зарегистрировались!";
 
-			$data = [ [$password, '0', '1', '0', '0', '0', 'zzzzzzzzzzzzzzzzzzzz'] ];
 
 			// open csv file for writing
 			$f = fopen("users/" . $filename, 'w');
+      $time = filemtime("users/" . $filename);
+      $data = [ [$password, '0', '1', '0', '0', '0', 'zzzzzzzzzzzzzzzzzzzz', $time] ];
 
 			if ($f === false) {
 				die('Error opening the file ' . $filename);
