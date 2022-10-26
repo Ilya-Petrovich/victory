bcd75c5b814c7af5532ebef2b438e858f309aa55
Liniza Khatipova <fanilovna03@gmail.com>
2022-10-26 15:20:43 +0300

    Change

--- login_page.php
+++ login_page.php
@@ -9,25 +9,26 @@
 	<title>Регистрация</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+    <!-- <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css"> -->
+	<link rel="stylesheet" href="../styles/style.css">
 </head>
-<body style='    background-image: url("images/Frame2.png");
+<body style='    background-image: url("../images/Frame2.png");
     background-position: center;
     background-size: cover;
     height: 843px;
     width: 1920px;
     background-color: #4C3375;
 '>
-	<main>
+	<main class="login">
 		<form action="login_script.php" method="post">
-	        <h1>Вход</h1>
-	        <div>
-	            <label for="username">Название команды:</label>
-	            <input type="text" name="username" id="username">
-				<label for="password">Пароль:</label>
-				<input type="password" name="password" id="password">
+	        <h1 class="input_i">Вход</h1>
+	        <div class="input_a">
+	            <!-- <label class="team" for="username">Название команды:</label> -->
+	            <input type="text" class="vvod_one" name="username" id="username" placeholder="Название команды">
+				<!-- <label class="pas" for="password">Пароль:</label> -->
+				<input type="password" class="vvod_one" name="password" id="password" placeholder="Пароль">
 	        </div>
-	        <button type="submit" name="submit">Войти</button>
+	        <button class="come_in" type="submit" name="submit">Войти</button>
 	    </form>
 	</main>
 </body>
--- registration_page.php
+++ registration_page.php
@@ -9,25 +9,26 @@
 	<title>Регистрация</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+    <!-- <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css"> -->
+	<link rel="stylesheet" href="../styles/style.css">
 </head>
-<body style='    background-image: url("images/Frame2.png");
+<body style='    background-image: url("../images/Frame2.png");
     background-position: center;
     background-size: cover;
     height: 843px;
     width: 1920px;
     background-color: #4C3375;
 '>
-	<main>
+	<main class="login">
 	    <form action="registration_script.php" method="post">
-	        <h1>Регистрация команды</h1>
-	        <div>
-	            <label for="username">Название команды:</label>
-	            <input type="text" name="username" id="username">
-				<label for="password">Пароль:</label>
-				<input type="password" name="password" id="password">
+	        <h1 class="input_i">Регистрация команды</h1>
+	        <div class="input_a">
+	            <!-- <label for="username">Название команды:</label> -->
+	            <input type="text" class="vvod_one" name="username" id="username" placeholder="Название команды">
+				<!-- <label for="password">Пароль:</label> -->
+				<input type="password" class="vvod_one" name="password" id="password" placeholder="Пароль">
 	        </div>
-	        <button type="submit" name="submit">Зарегистрировать</button>
+	        <button class="come_in_i"  type="submit" name="submit">Зарегистрировать</button>
 	    </form>
 </body>
 </html>
