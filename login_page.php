724153b6b6e95d8da0fbdc4842bea7efb9c10b79
Dasha Boxerka <ideliya3004@gmail.com>
2023-03-28 00:51:34 +0300

    Fixed login_page and registration_page.

--- login_page.php
+++ login_page.php
@@ -10,9 +10,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css"> -->
-	<link rel="stylesheet" href="../styles/style.css">
+	<link rel="stylesheet" href="./styles/style.css">
 </head>
-<body style='    background-image: url("../images/Frame2.png");
+<body style='    background-image: url("./images/Frame2.png");
     background-position: center;
     background-size: cover;
     height: 843px;
